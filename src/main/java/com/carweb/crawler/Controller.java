3c4bc6acd9c6627024a7128a0ded080de428940a
jumin <5216863@gmail.com>
2023-09-07 14:39:02 +0900

    add web - root

---
+++ src/main/java/com/carweb/crawler/Controller.java
@@ -0,0 +1,15 @@
+package com.carweb.crawler;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    public String hello(){
+
+        return "Hello World";
+    }
+}
